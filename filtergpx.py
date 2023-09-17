15206265c5195f67c2b1da73411e69197b67b7ab
lawrenceway65 <77293352+lawrenceway65@users.noreply.github.com>
2023-09-17 18:19:17 +0100

    Revert to use subprocess fpr locality query

--- filtergpx.py
+++ filtergpx.py
@@ -305,11 +305,14 @@ def get_locality(latitude, longitude):
     Using street level (zoom = 16) and picking second item, gives more accurate result
     """
     osm_request = "https://nominatim.openstreetmap.org/reverse?lat=%f&lon=%f&zoom=16&format=json"
-    result = requests.get(osm_request % (latitude, longitude))
+    result = subprocess.check_output(['curl', '-s', osm_request % (latitude, longitude)]).decode("utf-8")
+    result_json = json.loads(result)
+#    result = requests.get(osm_request % (latitude, longitude))
     # Extract second item from 'display_name'
     # Need to handle case where no locality - eg at sea
+#    print(result_json)
     try:
-        location = re.split(',', result.json()['display_name'])[1]
+        location = re.split(',', result_json['display_name'])[1]
     except IndexError:
         location = ""
     except KeyError:
