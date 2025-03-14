c106fd41ec614321fc3edcdf67874af1123ce90f
lawrenceway65 <77293352+lawrenceway65@users.noreply.github.com>
2025-03-14 18:46:27 +0000

    Correct to work on Mac

--- filtergpx.py
+++ filtergpx.py
@@ -436,6 +436,7 @@ if __name__ == "__main__":
     max_activities = config.max_activities
     activities_saved = 0
     status = State()
+    conv = Converter()
 
     print("Download activities from Garmin Connect.")
     try:
@@ -455,9 +456,10 @@ if __name__ == "__main__":
 
     for activity in activities:
         activity_id = activity["activityId"]
-        output_zip = '%sImport\\Raw\\activity.zip' % config.local_path
-        output_fit = '%sImport\\Raw\\%d_ACTIVITY.fit' % (config.local_path, activity_id)
-        output_gpx = '%sImport\\Raw\\activity.gpx' % config.local_path
+        sub_path = config.local_path + 'Import' + os.sep + "Raw" + os.sep  
+        output_zip = '%sactivity.zip' % sub_path
+        output_fit = '%s%d_ACTIVITY.fit' % (sub_path, activity_id)
+        output_gpx = '%sactivity.gpx' % sub_path
         # Only save and process if file not already saved from previous download
         if not os.path.isfile(output_fit):
             # Download the Garmin FIT file
@@ -467,7 +469,7 @@ if __name__ == "__main__":
                 zip_file.write(fit_data)
             # Extract fit data
             with zipfile.ZipFile(output_zip, 'r') as zip_ref:
-                zip_ref.extract('%d_ACTIVITY.fit' % activity_id, 'D:\\Documents\\GPSData\\Import\\Raw')
+                zip_ref.extract('%d_ACTIVITY.fit' % activity_id, sub_path)
             # Convert to gpx
             conv.fit_to_gpx(f_in=output_fit, f_out=output_gpx)
             with open(output_gpx, 'r') as gpx_file:
